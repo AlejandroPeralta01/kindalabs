e93452070f37c7b5df8c75ea8c37e2d118ca8173
AlejandroPeralta01 <alejandro.peralta07@gmail.com>
2025-02-27 13:41:34 -0300

    env

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from flask import Flask, request ,render_template
 import folium
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # Carga de variables de entorno
 
 app = Flask(__name__)
 
-url = "https://data.sfgov.org/resource/yitu-d5am.csv"
-coords = (37.7749, -122.4194) # coordenadas del centro de San Francisco, según google :)
-zoom = 12
-
 def load_data() -> pd.DataFrame:
+
     """
     Esta función carga los datos desde la URL, además realiza una limpieza de los datos
 
@@ -16,12 +17,13 @@ def load_data() -> pd.DataFrame:
         pd.DataFrame: Datos de las filmaciones ya filtrados
     """
 
-    df = pd.read_csv(url)
+    df = pd.read_csv(os.getenv("URL"))
 
     return df.dropna(subset=['locations','latitude','longitude'])
 
 
-def create_map(filtered_df : pd.DataFrame) -> tuple:
+def create_map(filtered_df : pd.DataFrame) -> str | None:
+
     """
     Crea un mapa usando Folium con marcadores para cada ubicación
 
@@ -29,36 +31,42 @@ def create_map(filtered_df : pd.DataFrame) -> tuple:
         HTML del mapa
     """
 
-    mapa = folium.Map(location=coords, zoom_start=zoom)
+    mapa = folium.Map(location=tuple(map(float,os.getenv("COORDS").split(','))),
+                      zoom_start=int(os.getenv("ZOOM"))
+                      )
 
     mapa_html = None
 
     if not filtered_df.empty:
 
-        for index , row in filtered_df.iterrows():
+        for _ , row in filtered_df.iterrows():
 
             latitude , longitude = float(row['latitude']) , float(row['longitude'])
 
-            popup_text = f"{row['title']} ({row.get('release_year','Desconocido')}) - {row['locations']}"
+            popup_text = f"{row['title']}<br>({row.get('release_year','N/A')})<br>{row['locations']}"
 
-            folium.Marker([latitude, longitude],popup=popup_text,tooltip=row['title']).add_to(mapa)
+            popup = folium.Popup(popup_text, max_width=300)
+
+            folium.Marker([latitude, longitude],popup=popup,tooltip=row['title']).add_to(mapa)
         
         mapa_html = mapa._repr_html_()
 
     return mapa_html
 
-
-movies_df= load_data()
+# Carga inicial de los datos
+movies_df= load_data() 
 
 @app.route('/', methods=['GET', 'POST'])
 
 def index() -> str:
+
     """
-    Renderiza la páguina principal con un mapa de ubicaciones de película
+    Renderiza la página principal con un mapa de ubicaciones de película
 
     Returns:
         str: Página HTML renderizada y posible mensaje de error
     """
+
     movie_name = request.form.get('movie','')
 
     message = None
@@ -68,6 +76,7 @@ def index() -> str:
         filtered_df = movies_df[movies_df['title'].str.contains(movie_name, case=False, na=False)]
 
         if filtered_df.empty:
+
             message = 'No se encuentraron resultados, ingrese un título válido'
 
     else:
@@ -79,7 +88,7 @@ def index() -> str:
     return render_template('index.html', mapa_html=mapa_html, movie_name=movie_name, message=message)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
 
 
 
